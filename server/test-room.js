bce78feef5c581b05fa8d7b54b57e35872149135
eayushsingh <ayushhsinghhe01@gmail.com>
2026-08-30 23:19:24 +0530

    chore: remove stray files

--- server/test-room.js
+++
@@ -1,5 +0,0 @@
-fetch('http://localhost:3000/api/rooms', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ name: 'Test Room', tags: [] })
-}).then(r => r.json()).then(console.log);
